7819529fd40fe7bd17166c19b776787fd7b750b6
Kritsanapon Rakkiatngam <kritsanapon.melo@mail.kmutt.ac.th>
2021-02-10 12:35:17 +0700

    [Add]Picture, [InProgress]loop&heart

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
     const app = {
         data() {
             return {
-                msg: 'Hello, Vue3'
+                tasks: [
+                    {title: 'COOKIERUN KINGDOM',done: false,
+                    img: '/images/cookiekingdom.png',done: false},
+                    {title: 'GENSHIN IMPACT', done: false,
+                    img: '/images/Genshin_Impact.png',done: false},
+                    {title: 'DOTA2', done: false,
+                    img: '/images/DOTA2.jpg',done: false},
+                ], 
+            }
+        },
+        methods: {
+            toggleDone(index){
+                this.tasks[index].done = !this.tasks[index].done
+            }
+        },
+        computed: {
+            countUndone(){
+                return this.tasks.filter( t => !t.done ).length
             }
         }
-
     }
     Vue.createApp(app).mount('#app')
+
